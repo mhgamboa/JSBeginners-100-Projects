c543e092a8d6eba6e214a2dfbaad68bb36ce1d95
Marcus <marcus.gamboa@gmail.com>
2020-10-26 16:06:37 -0600

    Still on project 8.
    I don't like learning bootstrap

--- Project 7 - Testimonials/js/script.js
+++ Project 7 - Testimonials/js/script.js
@@ -2,14 +2,53 @@ import { clients } from './client-reviews.js';
 
 const clientImage = document.querySelector('.client-image');
 const clientName = document.querySelector('.client-name');
-const clientStars = document.querySelector('.client-stars');
+const starsContainer = document.querySelector('.stars-container');
 const clientReview = document.querySelector('.client-review');
-let totalCustomers = clients.length;
+const rightArrow = document.querySelector('.fa-chevron-right');
+const leftArrow = document.querySelector('.fa-chevron-left');
+let totalCustomers = clients.length - 1;
 let currentCustomer = 0;
+
+//Function to add stars
+const star = document.createElement('i');
+star.classList.add('fas', 'fa-star')
+
+const halfStar = document.createElement('i');
+halfStar.classList.add('fas', 'fa-star-half');
+
+const createStars = () => {
+    starsContainer.innerHTML = '';
+    for(let x = clients[currentCustomer].stars; x > 0 ; x--) {
+        x == .5 
+        ? starsContainer.innerHTML += halfStar.outerHTML
+        : starsContainer.innerHTML += star.outerHTML;
+    }
+}
 
 //Once page loads it will show the first customer
 
-clientImage.style.background = `url('${clients[0].image}')`;
-clientName.textContent = clients[0].name;
-//clientStars = ??? 
-clientReview.textContent = clients[0].review;
+let cycleClients = () => {
+    clientImage.style.background = `url('${clients[currentCustomer].image}') center/cover`;
+    clientName.textContent = clients[currentCustomer].name;
+    createStars();
+    clientReview.textContent = clients[currentCustomer].review;
+};
+
+cycleClients();
+
+//When I click right Arrows it will go to the next client
+
+rightArrow.addEventListener('click', () => {
+    currentCustomer++;
+    currentCustomer > totalCustomers ? currentCustomer = 0 : null;
+
+    cycleClients();
+})
+
+//When I click left Arrow if will go to the previous client
+leftArrow.addEventListener('click', (e) => {
+    currentCustomer--;
+    currentCustomer < 0 ? currentCustomer = totalCustomers : null;
+
+    cycleClients();
+})
